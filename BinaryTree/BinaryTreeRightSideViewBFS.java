c8f57b7fcbf20ad8237f33055d25606ab9b8f4b4
Dherendra Dev <42056549+dev1031@users.noreply.github.com>
2024-06-26 23:51:46 +0530

    adding tree definition.

--- BinaryTree/BinaryTreeRightSideViewBFS.java
+++ BinaryTree/BinaryTreeRightSideViewBFS.java
@@ -1,3 +1,18 @@
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){
+        this.val = val;
+    }
+    public TreeNode(TreeNode left , TreeNode right , int val){
+        this.left = left;
+        this.right = right;
+        this.val = val;
+    }
+}
+
 public class BinaryTreeRightSideViewBFS{
     public static void main(String[] args){
         System.out.println("Hello World");
